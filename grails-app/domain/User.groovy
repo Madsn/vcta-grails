865e8e6d42910457ba3d9d6a14ac496d2320a246
Mikkel Madsen <m@madsn.net>
2014-04-01 22:39:19 +0200

    Remove "Team" from User model

--- grails-app/domain/User.groovy
+++ grails-app/domain/User.groovy
@@ -4,14 +4,4 @@
 class User extends grails.plugin.nimble.core.UserBase {
 
 	// Extend UserBase with your custom values here
-
-    Team team
-
-    static constraints = {
-        team nullable: true
-    }
-
-    String toString() {
-        return "Person " + id + ": " + name
-    }
 }
