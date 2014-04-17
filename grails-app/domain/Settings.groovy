2ef0af95760cb7244483eaeb3ce5d9848e7bc51e
Mikkel Madsen <m@madsn.net>
2014-04-17 15:35:47 +0200

    add some extra values to settings row - for future use

--- grails-app/domain/Settings.groovy
+++ grails-app/domain/Settings.groovy
@@ -2,5 +2,9 @@
 class Settings {
 	
 	boolean manageAllowed
+	String dashboardmsg // static on dashboard
+	String flashmsg // flash after login
+	String welcometext // login page
+	boolean frozen // read-only access for users
 
 }
