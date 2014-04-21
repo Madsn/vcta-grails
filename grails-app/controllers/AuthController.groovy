87edcb3de2879429dd42e2f09cb475a7b19c92ed
Mikkel Madsen <m@madsn.net>
2014-04-21 22:11:26 +0200

    Ignore nickname upper/lowercase in login form

---
+++ grails-app/controllers/AuthController.groovy
@@ -0,0 +1,83 @@
+/*
+ *  Nimble, an extensive application base for Grails
+ *  Copyright (C) 2010 Bradley Beddoes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.authc.AuthenticationException
+import org.apache.shiro.authc.DisabledAccountException
+import org.apache.shiro.authc.IncorrectCredentialsException
+import org.apache.shiro.authc.UsernamePasswordToken
+
+/**
+ * @author Bradley Beddoes
+ */
+class AuthController extends grails.plugin.nimble.core.AuthController {
+
+	@Override
+	def signin(String username, String password, String rememberme) {
+		def authToken = new UsernamePasswordToken(username.toUpperCase(), password)
+
+		if (rememberme) {
+			authToken.rememberMe = true
+		}
+
+		log.info("Attempting to authenticate user, $username. RememberMe is $authToken.rememberMe")
+
+		try {
+			SecurityUtils.subject.login(authToken)
+			userService.createLoginRecord(request)
+
+			def targetUri = session.getAttribute(AuthController.TARGET) ?: nimbleConfig.localusers.authentication.postLoginUrl
+			session.removeAttribute(AuthController.TARGET)
+
+			log.info("Authenticated user, $username")
+			if (userService.events["login"]) {
+				log.info("Executing login callback")
+				def newUri = userService.events["login"](authenticatedUser, targetUri, request)
+				if (newUri != null) {
+					targetUri = newUri
+				}
+			}
+			log.info("Directing to content $targetUri")
+			redirect(uri: targetUri)
+			return
+		}
+		catch (IncorrectCredentialsException e) {
+			log.info "Credentials failure for user '${username}'."
+			log.debug(e)
+
+			flash.type = 'error'
+			flash.message = message(code: "nimble.login.failed.credentials")
+		}
+		catch (DisabledAccountException e) {
+			log.info "Attempt to login to disabled account for user '${username}'."
+			log.debug(e)
+
+			flash.type = 'error'
+			flash.message = message(code: "nimble.login.failed.disabled")
+		}
+		catch (AuthenticationException e) {
+			log.info "General authentication failure for user '${username}'."
+			log.debug(e)
+
+			flash.type = 'error'
+			flash.message = message(code: "nimble.login.failed.general")
+		}
+		redirect(action: 'login')
+	}
+
+}
