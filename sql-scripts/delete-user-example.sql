db0321ea913f2794bcfbc99ebb3459b5f8456805
Mikkel Madsen <m@madsn.net>
2014-05-02 17:24:11 +0200

    Sample sql for deleting a user

---
+++ sql-scripts/delete-user-example.sql
@@ -0,0 +1,6 @@
+﻿delete from profile_base as p where p.owner_id = 273;
+delete from _group_users as g where g.user_base_id = 273;
+delete from _user_passwd_history as h where h.user_base_id = 273;
+delete from login_record as l where l.owner_id = 273;
+delete from permission as p where p.user_id = 273;
+delete from _user as u where u.id = 273;
